0a20a9442735762fae411e8506f25321de372a8e
Yunus Emre Eminler <yunus.eminler@gmail.com>
2024-03-10 23:32:58 +0300

    Authentication, login pages routes Added

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,12 @@
+const ClerkLayout =({
+    children
+}:{
+    children: React.ReactNode;
+})=>{
+    return(
+            <div className="flex flex-col items-center justify-center h-full">
+                {children}
+            </div>
+    );
+};
+export default ClerkLayout;
